eb677b92f7ee14e4b34f4de26e91d30337a755a1
jamesfan <rockets.james@163.com>
2019-07-22 22:23:37 +0800

    first successful submission

--- src/com/twosum/twosum2.java
+++ src/com/twosum/twosum2.java
@@ -3,19 +3,34 @@ import java.util.*;
 //faster version of the two sum
 //the runtime complexity of this algorithm is big o N;
 //the space complexity of the algorithm is big o N
+//first success version of submission
 class twosum2 {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> helper =new HashMap<>();
         int [] result=new int[2];
         int i=0;
         for(int e:nums){
-            helper.put(e,i++);
+            if(helper.containsKey(e)&&((2*e)==target)){
+                result[0]=helper.get(e);
+                result[1]=i;
+                return result;
+            }
+            else if(helper.containsKey(e)&&((2*e)!=target)){
+                helper.remove(e);
+                i+=1;
+                continue;
+            }
+            helper.put(e,i);
+            i+=1;
+
         }
         for(int e:nums){
             if(helper.containsKey(target-e)){
-                result[0]=helper.get(e);
-                result[1]=helper.get(target-e);
-                break;
+                if(helper.get(e)!=helper.get(target-e)){
+                    result[0]=helper.get(e);
+                    result[1]=helper.get(target-e);
+                    break;
+                }
             }
         }
         return result;
